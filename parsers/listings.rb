81c2b9119d67018061b417f2785a06620687dcd2
selinamelia <sellinaamelia@gmail.com>
2021-08-20 20:38:56 +0700

    add listings file

---
+++ parsers/listings.rb
@@ -0,0 +1,20 @@
+nokogiri = Nokogiri.HTML(content)
+
+products = nokogiri.css('div.a-column div.a-span4')
+products.each do |product|
+    a_element = product.at_css('a.a-link-normal')
+    if a_element
+        url = URI.join('https://www.amazon.com/', a_element['href']).to_s
+        # url = a_element['href'].gsub(/&qid=[0-9]*/,'')
+        if url =~ /\Ahttps?:\/\//i
+            pages << {
+                url: url,
+                page_type: 'products',
+                vars: {
+                    category: page['vars']['category'],
+                    url: url
+                }
+            }
+        end
+    end
+end
